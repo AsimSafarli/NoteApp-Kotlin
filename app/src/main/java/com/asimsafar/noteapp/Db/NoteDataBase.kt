7b4089dacd46dc6beb8f5e286d4c920d75f6f141
Asim <saffarrli14@gmail.com>
2024-01-02 19:20:03 +0400

    add fragment, database ,notefragment

--- app/src/main/java/com/asimsafar/noteapp/Db/NoteDataBase.kt
+++ app/src/main/java/com/asimsafar/noteapp/Db/NoteDataBase.kt
@@ -1,9 +1,12 @@
 package com.asimsafar.noteapp.Db
 
 import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.asimsafar.noteapp.Daos.NoteDao
 import com.asimsafar.noteapp.Module.Note
 
 
 @Database(entities = [Note::class], version = 1)
-abstract class AppDatabase {
+abstract class NoteDataBase:RoomDatabase() {
+    abstract  fun noteDao():NoteDao
 }
